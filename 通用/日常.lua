e09e030a53c82c9e9d0383073a962cbc769e0ebe
ReduckProject <reduckproject@gmail.com>
2024-05-28 09:34:35 +0800

    feat # 自动跑商

--- 通用/日常.lua
+++ 通用/日常.lua
@@ -3,6 +3,7 @@ load("Macro/Lib_Base.lua")
 load("Macro/Lib_PVX.lua")
 
 local f = {}
+local goodsExchangeCount = 0
 function Main()
     local map = map();
 
@@ -15,11 +16,39 @@ function Main()
     end
 
     if map == "马嵬驿" or map == "龙门荒漠" or map == "巴陵县" or map == "洛道" then
-        g_map_move["移动"]()
+        if goodsExchangeCount < 3 then
+            clickButton("Topmost/MB_on_switch_map_sure/Wnd_All/Btn_Option1/")
+            clickButton("Normal/OldQuestAcceptPanel/Btn_Sure/")
+            local nid = npc("名字:据点总管", "角色距离<8")
+            if nid ~= 0 then
+                interact(nid)
+                dialog("据点贸易！恶人谷")
+            end
+
+            --if buff("骑御") then
+            --    cast(54)
+            --end
+
+            if buff("据点贸易") and nobuff("骑御") then
+                cast(53)
+                return
+            end
+
+            g_map_move["移动"]()
+        end
     end
 
     if map == "枫叶泊·乐苑" or map == "帮会领地" or map == "广陵邑" then
         g_pvx["钓鱼"]()
+    end
+end
+
+
+
+function OnMessage(szMsg, szType)
+    --output(szMsg)
+    if szMsg.find(szMsg,"你已完成了") and szMsg.find(szMsg,"据点贸易") then
+        goodsExchangeCount = goodsExchangeCount + 1
     end
 end
 
